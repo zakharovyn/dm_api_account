486aba41b933d9d05c6ea932d8fddfac9ddeed79
Zakharov Yan <banjumingray@bk.ru>
2023-12-02 01:01:49 +0700

    dm_api_account

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 REQUIRES = [
     'requests',
-    'allure-pytest',
-    'restclient'
+    'allure-pytest'
 ]
 
 setup(
     name='dm_api_account',
     version='0.0.1',
-    packages=['dm_api_account'],
+    packages=['dm_api_account', 'dm_api_account.apis', 'dm_api_account.models'],
     url='https://github.com/zakharovyn/dm_api_account.git',
     license='MIT',
     author='yanzakharov',
